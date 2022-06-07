45fc41d9dfa5474bea740908b8b3bb0952f617d6
Mark Gerken <mgerken86@gmail.com>
2022-06-07 09:15:16 -0600

    Connected middleware and listener to PORT, finished initial server.js

--- server.js
+++ server.js
@@ -46,6 +46,29 @@ const fruitsSchema = new Schema({
 
 // make fruit model
 const Fruit = model("Fruit", fruitsSchema);
-console.log(fruitsSchema)
-console.log(Fruit)
+
+
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// Create our Express Application Object Bind Liquid Templating Engine
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
+
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
 
