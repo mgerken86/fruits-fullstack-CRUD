a2427c565c6131e59ace99bcbc5a77857f82f4b2
Mark Gerken <mgerken86@gmail.com>
2022-06-07 09:57:19 -0600

    Created partials layout page and index route

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ const CONFIG = {
 mongoose.connect(DATABASE_URL, CONFIG)
 
 // Events for when connection opens/disconnects/errors
-mongoose.connection 
-.on('open', () => console.log('Connected to Mongoose'))
-.on('close', () => console.log('Disconnected from Mongoose'))
-.on('error', () => console.log(error))
+mongoose.connection
+    .on('open', () => console.log('Connected to Mongoose'))
+    .on('close', () => console.log('Disconnected from Mongoose'))
+    .on('error', () => console.log(error))
 
 
 ////////////////////////////////////////////////
@@ -39,9 +39,9 @@ const { Schema, model } = mongoose;
 
 // make fruits schema
 const fruitsSchema = new Schema({
-  name: String,
-  color: String,
-  readyToEat: Boolean,
+    name: String,
+    color: String,
+    readyToEat: Boolean,
 });
 
 // make fruit model
@@ -51,10 +51,7 @@ const Fruit = model("Fruit", fruitsSchema);
 /////////////////////////////////////////////////
 // Create our Express Application Object Bind Liquid Templating Engine
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// Create our Express Application Object Bind Liquid Templating Engine
-/////////////////////////////////////////////////
-const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
+const app = require("liquid-express-views")(express(), { root: [path.resolve(__dirname, 'views/')] })
 
 
 /////////////////////////////////////////////////////
@@ -72,3 +69,36 @@ app.use(express.static("public")); // serve files from public statically
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
 
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.get("/", (req, res) => {
+    res.send("your server is running... better catch it.");
+});
+
+app.get("/fruits/seed", (req, res) => {
+    // array of starter fruits
+    const startFruits = [
+        { name: "Orange", color: "orange", readyToEat: false },
+        { name: "Grape", color: "purple", readyToEat: false },
+        { name: "Banana", color: "orange", readyToEat: false },
+        { name: "Strawberry", color: "red", readyToEat: false },
+        { name: "Coconut", color: "brown", readyToEat: false },
+    ];
+
+    // Delete all fruits
+    Fruit.deleteMany({}).then((data) => {
+        // Seed Starter Fruits
+        Fruit.create(startFruits).then((data) => {
+            // send created fruits as response to confirm creation
+            res.json(data);
+        });
+    });
+});
+
+// Index Route
+app.get('/fruits', (req, res) => {
+    const fruits = await Fruit.find({})
+    res.render('/fruits/index.liquid', { fruits })
+})
